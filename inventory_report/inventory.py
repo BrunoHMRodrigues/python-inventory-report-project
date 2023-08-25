f4d65c6c42583d1dc91d983863e45992dc9bb9b4
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-08-25 14:15:21 -0300

    Feature: Requisite 05 Completed

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
