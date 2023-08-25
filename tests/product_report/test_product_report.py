53bf4f07578145f224c376b37719ba1efacf9d5d
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-08-25 13:35:44 -0300

    Feature: Requisite 02 Completed

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,29 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product_data = {
+        "id": "123",
+        "company_name": "Company Name",
+        "product_name": "Product Name",
+        "manufacturing_date": "2023-08-25",
+        "expiration_date": "2024-08-25",
+        "serial_number": "SN123456",
+        "storage_instructions": "Store in a cool place."
+    }
+
+    product = Product(**product_data)
+
+    expected_output = (
+        f"The product {product.id} - {product.product_name}"
+        " with serial number "
+        f"{product.serial_number} manufactured on {product.manufacturing_date}"
+        " by the "
+        f"company {product.company_name} valid until {product.expiration_date}"
+        " must be stored "
+        f"according to the following instructions: "
+        f"{product.storage_instructions}."
+
+    )
+
+    assert str(product) == expected_output
